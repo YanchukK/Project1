f180880717d3cc3711b735c3786496c794a2b0ea
Kate <32255660+YanchukK@users.noreply.github.com>
2019-02-18 18:19:26 +0200

    Update Fighter.cs

--- HW/Fighter.cs
+++ HW/Fighter.cs
@@ -22,59 +22,13 @@ namespace ConsoleApp1
                 this.Health = health;
                 this.DamagePerAttack = damage;
             }
-            
-            // В результате удара у бойца отнимается столько жизней,
-            // какой силой удара обладает нападающий
-            public int Strike(Fighter fighter1, Fighter fighter2) // нанести удар
-            {
-                return fighter1.Health -= fighter2.DamagePerAttack;
-            }
-
-
-            Random rnd = new Random();
-
-            //Получить очередное (в данном случае - первое) случайное число
-            //int value = rnd.Next();
-            
-
-            public Fighter Battle(Fighter fighter1, Fighter fighter2)
-            {
-                // рандомно выбрать нападающего
-                int value = rnd.Next(0, 2);
-                // через тики
-                
-                if (value == 0)
-                {
-                    do
-                    {
-                        Strike(fighter1, fighter2);
-                    } while (fighter1.Health > 0 && fighter2.Health > 0);
-                }
-                else
-                {
-                    do
-                    {
-                        Strike(fighter1, fighter2);
-                    } while (fighter1.Health > 0 && fighter2.Health > 0);
-                }
-
-
-
-
-
-                if (fighter1.Health > 0)
-                {
-                    return fighter2; // победитель
-                }
-                else
-                    return fighter1;                
-            }
         }
-        
+
+
         // Написать метод, который определяет победителя схватки двух бойцов.​
 
         // Схватка происходит следующим образом: случайным образом определяется
-        // первый нападающий, после чего он наносит удар. Далее бойца наносят удары по очереди.​
+        // первый нападающий, после чего он наносит удар.Далее бойца наносят удары по очереди.​
 
         //В результате удара у бойца отнимается столько жизней, какой силой удара обладает нападающий.​
         //Проигравшим считается тот, у кого уровень жизней меньше или равно 0.​
@@ -82,19 +36,89 @@ namespace ConsoleApp1
 
 
 
-
-        static void Main(string[] args)
+        public static class Battle
         {
-            //Создание объекта для генерации чисел
-            Random rnd = new Random();
+            public static int Strike(Fighter fighter1, Fighter fighter2) // нанести удар
+            {
+                // В результате удара у бойца отнимается столько жизней,
+                // какой силой удара обладает нападающий
+                return fighter1.Health -= fighter2.DamagePerAttack;
+            }
 
             //Получить очередное (в данном случае - первое) случайное число
             //int value = rnd.Next();
-            int value = rnd.Next(0, 2);
-            //Вывод полученного числа в консоль
-            Console.WriteLine(value);
 
-            //Console.WriteLine();
+
+            public static Fighter BattleF(Fighter fighter1, Fighter fighter2)
+            {
+                // рандомно выбрать нападающего
+                Random rnd = new Random();
+                int value = rnd.Next(0, 2);
+                // через тики
+
+                int tick = 0;
+
+                if (value == 0)
+                {
+                    do
+                    {
+                        if (tick == 0)
+                        {
+                            Strike(fighter1, fighter2);
+                            tick++;
+                        }
+                        else
+                        {
+                            Strike(fighter2, fighter1);
+                            tick++;
+                        }
+
+                    } while (fighter1.Health > 0 && fighter2.Health > 0);
+                }
+                else
+                {
+                    do
+                    {
+                        if (tick == 0)
+                        {
+                            Strike(fighter1, fighter2);
+                            tick++;
+                        }
+                        else
+                        {
+                            Strike(fighter2, fighter1);
+                            tick++;
+                        }
+                    } while (fighter1.Health > 0 && fighter2.Health > 0);
+                }
+                
+
+                if (fighter1.Health > 0)
+                {
+                    return fighter2; // победитель
+                }
+                else
+                    return fighter1;
+            }
+        }
+        
+
+
+
+        static void Main(string[] args)
+        {
+            var fighter1 = new Fighter("fighter1", 10, 2);
+            var fighter2 = new Fighter("fighter2", 10, 5);
+
+            //var battle = new Battle();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((Battle.BattleF(fighter1, fighter2)).Name);
+            }
+            
+
+            Console.WriteLine();
         }
     }
 }
