f6f4fc2c28f6760ec3d9108b96b6ef57ee9d09d8
Kate <32255660+YanchukK@users.noreply.github.com>
2019-02-25 14:22:29 +0200

    Create Program.cs

---
+++ HW/Registration/Program.cs
@@ -0,0 +1,12 @@
+namespace Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Registration registration = new Registration();
+
+            registration.RegisterUser();
+        }
+    }
+}
