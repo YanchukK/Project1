17f83c0fc4820b0710178998d4600659e08a7f6b
Kate <32255660+YanchukK@users.noreply.github.com>
2019-03-06 18:54:27 +0200

    Create Manager.cs

---
+++ HW/ORM/Manager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithDB
+{
+    public class Manager : Entity
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
